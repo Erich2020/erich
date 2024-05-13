3e0265fb5e5328d0086ac9adbd403a5f9292b953
Erich.lz <erich202056@gmail.com>
2024-05-12 22:10:17 -0600

    added site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://erich2020.github.io/",
   integrations: [tailwind(), astroI18next()],
 });
