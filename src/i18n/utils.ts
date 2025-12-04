9b73c90af4d68fd704e30147048b24e3cde2c5f6
Erick <erick.lopez@globalprimex.xyz>
2025-12-04 00:07:23 -0600

    feat. add log

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,19 +1,20 @@
-import { ui, defaultLang, showDefaultLang } from './ui';
+import { ui, defaultLang, showDefaultLang } from "./ui";
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/erich/');
+  const [, lang] = url.pathname.split("/erich/");
+  console.log("Extracted lang from URL:", lang, url.pathname);
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
 
 export function useTranslations(lang: keyof typeof ui) {
-  return function t(key: keyof typeof ui[typeof defaultLang]) {
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
-  }
+  };
 }
 
 export function useTranslatedPath(lang: keyof typeof ui) {
   return function translatePath(path: string, l: string = lang) {
-    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`
-  }
+    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`;
+  };
 }
